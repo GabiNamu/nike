9593715c86a41ef6b2912038d2b46b0ad7faca74
Gabriela Muniz <gabrielanamu@live.com>
2023-10-17 18:13:28 -0300

    nike website

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import Hero from "./Hero";
+
+export {
+  CustomerReviews,
+  Subscribe,
+  Footer,
+  SuperQuality,
+  SpecialOffers,
+  Services,
+  PopularProducts,
+  Hero
+}
